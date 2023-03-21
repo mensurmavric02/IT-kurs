9965007faa702d75c7ef38bd91a858e46a4ba8a1
mensurmavric02 <mesnur.mavric1@gmail.com>
2023-03-21 21:05:17 +0100

    featching API

--- async js/app.js
+++ async js/app.js
@@ -14,17 +14,25 @@
 //-------------> PROMISE <---------------
 //Promise je objekat koji se vraca nakon asihrone operacije.
 
-const promise = new Promise((resolve, reject) => {
-    const number = 3
-    if(1 + 2 == number){
-        resolve("Success")
-    }else{
-        reject("Fail")
-    }
-})
+// const promise = new Promise((resolve, reject) => {
+//     const number = 3
+//     if(1 + 2 == number){
+//         resolve("Success")
+//     }else{
+//         reject("Fail")
+//     }
+// })
 
-promise.then((message) => {
-    console.log("Ovo je " + message)
-}).catch((error) => {
-    console.log("Ovo je " + error)
-})
+// promise.then((message) => {
+//     console.log("Ovo je " + message)
+// }).catch((error) => {
+//     console.log("Ovo je " + error)
+// })
+
+// fetching API
+const fetchingData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error)) 
+}
