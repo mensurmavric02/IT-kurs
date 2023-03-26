bc82421d874b22674c496a9b0123217e5c4c0a14
mensurmavric02 <mesnur.mavric1@gmail.com>
2023-03-26 16:49:23 +0200

    OOT

---
+++ OOP/app.js
@@ -0,0 +1,75 @@
+// Definisanje klase
+// class Person {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//     }
+  
+//     // Definisanje metoda
+//     sayHello() {
+//       console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+//     }
+//   }
+  
+//   // Kreiranje novog objekta na osnovu klase
+//   const person1 = new Person('John', 30);
+  
+//   // Pozivanje metoda objekta
+//   person1.sayHello(); // Hello, my name is John and I'm 30 years old.
+
+// -----------------------------------------------------------------------------
+
+// class voce {
+//     constructor(name, weight){
+//         this.name = name
+//         this.weight = weight
+//     }
+//     sayHello(){
+//         console.log(`Zdravo, imamo ${this.name}. Cena je ${this.weight} din.`);
+//     }   
+// }
+// const vrsta = new voce ("jabuke", 130)
+
+// vrsta.sayHello()
+
+// -------------------------------------------------------------------------------
+
+// class vozilo {
+//     constructor(marka, godina, motor){
+//         this.marka = marka
+//         this.godina = godina
+//         this.motor = motor
+//     }
+//     da_li_je_motor_upaljen(){
+//         if(this.motor === true){
+//             console.log("Motor je upaljen");
+//         }else{
+//             console.log("Motor je ugasen");
+//         }
+//     }
+// }
+
+// const auto = new vozilo("BMW", 2005, true)
+
+// auto.da_li_je_motor_upaljen()
+
+// ------------------------------------------------------------------------------
+
+// class politicar{
+//     constructor(name, years, vlast){
+//         this.name = name
+//         this.years = years
+//         this.vlast = vlast
+//     }
+//     da_li_je_na_vlasti(){
+//         if(this.vlast === true){
+//             console.log(`${this.name}, da na vlasti je :(`);
+//         }else{
+//             console.log("nije na vlasti :)");
+//         }
+//     }
+// }
+ 
+// const poliTicatri = new politicar("Vucic", 53, true)
+
+// poliTicatri.da_li_je_na_vlasti()
--- async js/app.js
+++ async js/app.js
@@ -30,9 +30,47 @@
 // })
 
 // fetching API
-const fetchingData = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error)) 
-}
+// const fetchingData = () => {
+//     fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error)) 
+// }
+
+// funkcija koja sadrzi lazni objekat
+// id:
+// ime
+// godine
+// email adress
+
+// const fetchingData = async () => {
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//         console.log(response);
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// fetchingData()
+
+const generateFakeObject = async () => {
+    const fakeObject = {
+      id: Math.floor(Math.random() * 10) + 1,
+      name: 'John Doe',
+      age: 30,
+      email: 'johndoe@example.com'
+    };
+    return fakeObject;
+  }
+
+  const displayFakeObject = async () => {
+    try {
+      const fakeObject = await generateFakeObject();
+      console.log(fakeObject);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  displayFakeObject();
